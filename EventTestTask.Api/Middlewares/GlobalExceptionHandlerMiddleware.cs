6c2c988f10be5dc9d248bc269612c835aee3bd8f
Justemoi <144348479+Justemoie@users.noreply.github.com>
2025-05-27 21:25:38 +0300

    Add error logging

--- EventTestTask.Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
+++ EventTestTask.Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -1,7 +1,7 @@
 using System.Security.Authentication;
 using System.Text.Json;
 using BCrypt.Net;
-using Microsoft.AspNetCore.Http;
+using FluentValidation;
 
 namespace EventTestTask.Api.Middlewares;
 
@@ -17,63 +17,63 @@ public class GlobalExceptionHandlerMiddleware(
         }
         catch (Exception _) when (_ is OperationCanceledException or TaskCanceledException)
         {
-            logger.LogInformation("Operation was canceled");
+            logger.LogError("Operation was canceled");
             context.Response.StatusCode = StatusCodes.Status400BadRequest;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize(_.Message));
         }
         catch (Exception _) when (_ is KeyNotFoundException)
         {
-            logger.LogInformation("Not found");
+            logger.LogError("Not found");
             context.Response.StatusCode = StatusCodes.Status404NotFound;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize(_.Message));
         }
         catch (Exception _) when (_ is AuthenticationException)
         {
-            logger.LogInformation("Authentication failed");
+            logger.LogError("Authentication failed");
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize(_.Message));
         }
         catch (Exception _) when (_ is UnauthorizedAccessException)
         {
-            logger.LogInformation("Unauthorized");
+            logger.LogError("Unauthorized");
             context.Response.StatusCode = StatusCodes.Status403Forbidden;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize(_.Message));
         }
         catch (Exception _) when (_ is NullReferenceException)
         {
-            logger.LogInformation("NullReference");
+            logger.LogError("NullReference");
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize(_.Message));
         }
         catch (Exception _) when (_ is InvalidDataException)
         {
-            logger.LogInformation("Invalid data");
+            logger.LogError("Invalid data");
             context.Response.StatusCode = StatusCodes.Status400BadRequest;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize(_.Message));
         }
         catch (Exception _) when (_ is InvalidOperationException)
         {
-            logger.LogInformation("Invalid operation");
+            logger.LogError("Invalid operation");
             context.Response.StatusCode = StatusCodes.Status404NotFound;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize(_.Message));
         }
-        catch (Exception _) when (_ is FluentValidation.ValidationException)
+        catch (Exception _) when (_ is ValidationException)
         {
-            logger.LogInformation("FluentValidation exception");
+            logger.LogError("FluentValidation exception");
             context.Response.StatusCode = StatusCodes.Status400BadRequest;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize(_.Message));
         }
         catch (Exception _) when (_ is SaltParseException)
         {
-            logger.LogInformation("Salt parse exception");
+            logger.LogError("Salt parse exception");
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonSerializer.Serialize("Invalid password or email"));
